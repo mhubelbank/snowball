761d2d9d12db463774327358e481c864991240ca
M-Allahham <allahham.malik@gmail.com>
2022-08-08 00:04:32 -0400

    changed variable names with underscores included pydoc strings just 4 mara fstring now bc mara hates concatenation (the cat part probably @toby)

--- apps/home/logic/cost_of_living_calculator.py
+++ apps/home/logic/cost_of_living_calculator.py
@@ -2,36 +2,49 @@ import re
 import requests
 import pandas as pd
 
-# This function uses the MIT Cost of Living calculator to find the expected cost of living based on family size
-# Arguments:
-#   city            - The city that you are living in
-#   state           - The state you are living in
-#   total_adults    - The total number of adults (1 or 2)
-#   working_adults  - The number of adults who work (1 or 2)
-#   children        - The number of children (currently 3 max, can extrapolate higher I think(?))
-# Returns:
-#   Estimated cost of living after taxes as a float
-def get_cost_of_living(city, state, total_adults, working_adults, children):
+def get_cost_of_living(city: str, state: str, total_adults: int, working_adults: int, children: int) -> float:
+    """This function uses the MIT Cost of Living calculator to find the expected cost of living based on family size
+
+    Parameters
+    ----------
+    city : str
+        The city that you are living in
+    state : str
+        The state you are living in
+    total_adults : int
+        The total number of adults (1 or 2)
+    working_adults : int
+        The number of adults who work (1 or 2)
+    children : int
+        The number of children (currently 3 max, can extrapolate higher I think(?))
+
+    Returns
+    -------
+    float
+        Estimated cost of living after taxes as a float
+    """
 
     if children > 3:
         return "Too many children!"
 
     # URL to get the latitude and longitude of a city
-    LATLONGURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + ",US&limit=1&appid=d375f2270b268435bedceb0d3bff9de6"
+    LAT_LONG_URL = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},US&limit=1&appid=d375f2270b268435bedceb0d3bff9de6"
 
-    response = requests.get(url=LATLONGURL)
+    response = requests.get(url=LAT_LONG_URL)
 
     # URL to get the FIPs code of a particular latitude and longitude
-    FIPSURL = "https://geo.fcc.gov/api/census/area?lat=" + str(response.json()[0]["lat"]) + "&lon=" + str(response.json()[0]["lon"]) + "&censusYear=2020&format=json"
+    latitude = response.json()[0]["lat"]
+    longitude = response.json()[0]["lon"]
+    FIPS_URL = f"https://geo.fcc.gov/api/census/area?lat={latitude}&lon={longitude}&censusYear=2020&format=json"
 
-    response2 = requests.get(FIPSURL)
+    response2 = requests.get(FIPS_URL)
 
     fips_code = response2.json()["results"][0]["county_fips"]
 
     # URL to get the cost of living tables from MIT Cost of Living calculator based on the FIPs code
-    MITURL = "https://livingwage.mit.edu/counties/" + str(fips_code)
+    MIT_URL = f"https://livingwage.mit.edu/counties/{fips_code}"
 
-    tables = pd.read_html(MITURL)
+    tables = pd.read_html(MIT_URL)
 
     if total_adults == 1:
         # Cost of living will always be in the second table on the page
--- apps/home/logic/income_tax_calculator.py
+++ apps/home/logic/income_tax_calculator.py
@@ -1,18 +1,29 @@
 import requests
 
-# This function takes in tax-relevant parameters and returns the effective tax rate
-# Arguments: 
-#   money      - This is the income that will be taxed at a regular income tax rate. 
-#   city       - The city you are living in
-#   state      - The state you are living in
-#   married    - Whether you are married or not
-#   dependents - The number of dependents that you/your partner support (this typically doesn't matter, depends on the state)
-# Returns:
-#   The effective tax rate based on the arguments as a float
-def get_effective_income_tax_rate(money, city, state, married=0, dependents=1):
+def get_effective_income_tax_rate(money: int, city: str, state: str, married=False, dependents=1) -> float:
+    """This function takes in tax-relevant parameters and returns the effective tax rate
+
+    Parameters
+    ----------
+    money : int
+        This is the income that will be taxed at a regular income tax rate. 
+    city : str
+        The city you are living in
+    state : str
+        The state you are living in
+    married : bool, optional
+        Whether you are married or not, by default False
+    dependents : int, optional
+        The number of dependents that you/your partner support (this typically doesn't matter, depends on the state), by default 1
+
+    Returns
+    -------
+    float
+        The effective tax rate based on the arguments
+    """
 
     # This is the URL that the smartasset income tax calculator references for its API
-    URL = "https://smartasset.com/taxes/income-taxes?render=json&ud-current-location=CITY%7C" + city + "%7C" + state + "&ud-it-household-income=" + str(money) + "&ud-married=" + str(married) + "&ud-it-personal-exemptions=" + str(dependents)
+    URL = f"https://smartasset.com/taxes/income-taxes?render=json&ud-current-location=CITY%7C{city}%7C{state}&ud-it-household-income={money}&ud-married={married}&ud-it-personal-exemptions={dependents}"
 
     # Headers so the request doesn't use the cached information in browser
     headers = {
